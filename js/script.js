626f9a81c8c6119bf1616fc5ef498e93fc6d155f
zeratul <e.kovshilov@gmail.com>
2019-03-21 19:37:40 +0300

    исправил замечания

--- js/script.js
+++ js/script.js
@@ -1,102 +1,103 @@
-// Карта на сайте
-ymaps.ready(init); // вызов функции
-//events
-function init() { // определение функции 
-	var map = new ymaps.Map('map', { //создаем новый конструктор для карты
-		center: [59.9386, 30.3231], //определяем центр карты (http://webmap-blog.ru/tools/getlonglat-ymap2.html)
-		zoom: 17, // определяем уровеь масштабирования
-		controls: ['zoomControl'], // выбираем какие элементы управления отоброжать
-		behaviors: ['drag'] // отключаем масштабирование - включаем перетаскивание
-	});
-	//Добавляем всплывающую подсказку (hint)
-	var placemark = new ymaps.Placemark([59.9386, 30.3231], {
-		hintContent: 'NERDS' //контент хинта
-	},
-		//изображение хинта
-		{
-			iconLayout: 'default#image', //название
-			iconImageHref: 'img/marker.png', //источник
-			iconImageSize: [238, 190], //размер 
-			iconImageOffset: [-40, -140] //координаты смещения
-		});
-	//вызываем метку с помощью коллекции geoObjects
-	map.geoObjects.add(placemark);
+// Yandex Map ===================================
+ymaps.ready(init);
+
+function init() {
+  var map = new ymaps.Map('map', {
+    center: [59.9386, 30.3231],
+    zoom: 17,
+    controls: ['zoomControl'],
+    behaviors: ['drag']
+  });
+  var placemark = new ymaps.Placemark([59.9386, 30.3231], {
+    hintContent: 'NERDS'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/marker.png',
+    iconImageSize: [238, 190],
+    iconImageOffset: [-40, -140]
+  });
+  map.geoObjects.add(placemark);
 };
 
-
-/*Модальное окно*/
-
-var overlay = document.querySelector(".overlay"),
-	modalOpen = document.querySelector(".call-modal"),
-	modalClose = document.querySelector(".modal__exit");
-modalOpen.addEventListener("click", function (event) {
-	event.preventDefault();
-	overlay.style.display = "flex";
-});
-modalClose.addEventListener("click", function (event) {
-	event.preventDefault();
-	overlay.style.display = "none";
-});
-window.addEventListener("click", function (event) {
-	if (event.target === overlay) {
-		overlay.style.display = "none";
-	}
-});
-window.addEventListener("keydown", function (event) {
-	if (event.keyCode === 27) {
-		overlay.style.display = "none";
-	}
-});
+// Modal =====================================
+// variables
+var overlay = document.querySelector(".overlay");
+var modalOpen = document.querySelector(".call-modal");
+var modalClose = document.querySelector(".modal__exit");
 var form = document.querySelector(".modal-form");
 var email = document.querySelector('input[type="email"]');
+// events
+// show modal
+modalOpen.addEventListener("click", function (event) {
+  event.preventDefault();
+	overlay.style.display = "flex";
+	document.body.style.overflow = "hidden";
+	// add animation fadeIn
+	document.querySelector(".modal").classList.add("fadeIn")
+	// remove animation fadeIn
+  setTimeout(() => {
+    document.querySelector(".modal").classList.remove("fadeIn")
+  }, 1000);
+});
+// close modal
+modalClose.addEventListener("click", function (event) {
+  event.preventDefault();
+	overlay.style.display = "none";
+	document.body.style.overflow = "unset";
+});
+// close modal when we click over our modal area
+window.addEventListener("click", function (event) {
+  if (event.target === overlay) {
+		overlay.style.display = "none";
+		document.body.style.overflow = "unset";
+  }
+});
+// close modal when we press Escape 
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+		overlay.style.display = "none";
+		document.body.style.overflow = "unset";
+  }
+});
+// validation
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-
-	if (email.value === "") {
+	// if field is empty
+  if (email.value === "") {
+		// show error 
 		email.classList.add("modal-form__input_error");
-	}
-
-	setTimeout(function () {
-		email.classList.remove("modal-form__input_error");
-	}, 3000);
+		// add animation shake
+		document.querySelector(".modal").classList.add("snake")
+		// remove animation shake and error
+    setTimeout(() => {
+			document.querySelector(".modal").classList.remove("snake");
+			email.classList.remove("modal-form__input_error");
+    }, 1000);
+  }
 });
 
-
+// Slider ==========================================
 if (document.body.classList.contains('index-body')) {
-	/* Слайдер */
-	var dots = document.querySelectorAll(".dot");
-	var slides = document.querySelectorAll(".hero-list li");
+  // variables
+  var dots = document.querySelectorAll(".dot");
+  var slides = document.querySelectorAll(".hero-list li");
 
-	var _loop = function _loop(i) {
-		dots[i].addEventListener("click", function () {
-			document.querySelector(".dot.selected").classList.remove("selected");
-			dots[i].classList.add("selected");
+  var _loop = function _loop(i) {
+    dots[i].addEventListener("click", function () {
+      document.querySelector(".dot.selected").classList.remove("selected");
+      dots[i].classList.add("selected");
 
-			for (var j = 0; j < slides.length; j++) {
-				document
-					.querySelector(".hero-list li.selected")
-					.classList.remove("selected");
-				slides[i].classList.add("selected");
-			}
-		});
-	};
+      for (var j = 0; j < slides.length; j++) {
+        document
+          .querySelector(".hero-list li.selected")
+          .classList.remove("selected");
+        slides[i].classList.add("selected");
+      }
+    });
+  };
 
-	for (var i = 0; i < dots.length; i++) {
-		_loop(i);
-	}
+  for (var i = 0; i < dots.length; i++) {
+    _loop(i);
+  }
 
 }
-
-function numberLimitation (inp, limit) {
-    var t = typeof inp === "string" ? document.querySelector (inp) : inp,
-    f = function (e) {
-        var v = t.value.split ("");
-        if (v.length > limit) {
-              t.value = v.slice(0, limit).join("");
-        }
-    };
-    t.addEventListener ("input", f);
-}
-window.addEventListener ("load", function () {
-  numberLimitation ("#inp", 5);
-})
